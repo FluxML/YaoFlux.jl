3a67bfb8d8927548e6da41de666fd975a0218b40
Roger-luo <hiroger@qq.com>
2019-05-16 17:26:24 -0400

    add more adjoints

--- src/YaoFlux.jl
+++ src/YaoFlux.jl
@@ -1,12 +1,31 @@
 module YaoFlux
 
-using Zygote, LuxurySparse, Yao, YaoBase
-import Zygote: @adjoint
+using Zygote, LuxurySparse, Yao, YaoBase, SparseArrays, BitBasis
+import Zygote: Context
 
 Zygote.@adjoint (::Type{T})(perms, vals) where T <: PermMatrix = T(perms, vals), Δ -> nothing
 Zygote.@adjoint (::Type{T})() where T <: IMatrix = T(), Δ -> nothing
 Zygote.@adjoint Base.:(*)(A::Number, B::PermMatrix) = A * B, Δ->(sum(Δ .* B), A * Δ)
 Zygote.@adjoint Base.:(*)(A::PermMatrix, B::Number) = A * B, Δ->(A * Δ, sum(Δ .* B))
+
+Zygote.@adjoint SparseArrays.SparseMatrixCSC(A::PermMatrix) = SparseMatrixCSC(A), Δ->(Δ, )
+
+Zygote.@adjoint BitBasis.onehot(::Type{T}, nbits::Int, x::Integer, nbatch::Int) where T = onehot(T, nbits, x, nbatch), Δ -> nothing
+
+# upstreams
+Zygote.@adjoint Base.:(-)(a, b) = a-b, Δ -> (Δ, -Δ)
+Zygote.@adjoint Base.:(+)(a, b) = a+b, Δ -> (Δ, Δ)
+Zygote._forward(cx::Context, ::typeof(mapreduce), f, op, xs) = Zygote._forward(cx, x->reduce(op, map(f, x)), xs)
+
+
+# require mutate
+Zygote.@adjoint! function copyto!(xs::AbstractVector, ys::Tuple)
+    xs_ = copy(xs)
+    copyto!(xs, ys), function (dxs)
+        copyto!(xs_, xs)
+        return (nothing, Tuple(dxs))
+    end
+end
 
 
 end # module
